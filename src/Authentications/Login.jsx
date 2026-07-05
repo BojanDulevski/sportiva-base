7333c4da470bd67c4883312254479252f0bdcf7d
Bojan Dulevski <bojan.deuleto7@gmail.com>
2026-07-05 15:33:24 +0200

    Fix import statement for SignUp.css

--- src/Authentications/Login.jsx
+++ src/Authentications/Login.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import './Signup.css';
+import './SignUp.css';
 import { Link, useNavigate } from 'react-router-dom';
 function Login() {
   const [formData, setFormData] = useState({
